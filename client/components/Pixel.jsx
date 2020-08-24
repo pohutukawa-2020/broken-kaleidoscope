e4df064144d28ba9f50066a681bbd8d618876ee4
m-silver <mattsilver@windowslive.com>
2020-08-25 10:41:50 +1200

    Add onMouseEnter handler to change colour to green

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -19,7 +19,7 @@ class Pixel extends React.Component {
   clickHandler = (event) => {
     const randomHexColor = () =>
     `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
-    
+
     this.setState({
       style: {
         height: '50px',
@@ -29,10 +29,19 @@ class Pixel extends React.Component {
     })
   }
 
+  hoverHandler = (event) => {
+    this.setState({
+      style: {
+        height: '50px',
+        width: '50px',
+        backgroundColor: 'green'
+      }
+    })
+  }
 
   render() {
     return (
-      <div style={this.state.style} onClick={this.clickHandler}></div>
+      <div style={this.state.style} onClick={this.clickHandler} onMouseEnter={this.hoverHandler}></div>
     )
   }
 }
