38b2eed21b0c302bea77ac19d91872a17d827e3d
michael-rua <michaelrua1@gmail.com>
2020-08-25 11:44:08 +1200

    add Handler function to change colours of pixels

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,29 +2,27 @@ import React from 'react'
 import Pixel from './Pixel'
 
 const App = () => {
-  
   return (
     <>
-   
-    <Pixel />
-    <Pixel />
-    <Pixel />
-    <Pixel />
-    <Pixel />
-    <Pixel />
-    <Pixel />
-    <Pixel />
-    <Pixel />
-    <Pixel />
-    <Pixel />
-    <Pixel />
-    <Pixel />
-    <Pixel />
-    <Pixel />
-    <Pixel />
-    <Pixel />
-    <Pixel />
-    <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
     </>
   )
 }
--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -15,7 +15,7 @@ class Pixel extends React.Component {
    randomHexColor = () =>
      `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
 
-    clickHandler = () => {
+    clickHandler = evt => {
       this.setState({
         style: {
           height: '100px',
@@ -23,10 +23,48 @@ class Pixel extends React.Component {
           backgroundColor: this.randomHexColor() }
       })
     }
+    onMouseEnter = evt => {
+      this.setState({
+        style: {
+          height: '100px',
+          width: '100px',
+          backgroundColor: 'green' }
+      })
+    }
+    onContextMenu = evt => {
+      evt.preventDefault()
+      this.setState({
+        style: {
+          height: '100px',
+          width: '100px',
+          backgroundColor: 'black' }
+      })
+    }
+    onDoubleClick = evt => {
+      this.setState({
+        style: {
+          height: '100px',
+          width: '100px',
+          backgroundColor: 'white' }
+      })
+    }
+    onDragEnter = evt => {
+      this.setState({
+        style: {
+          height: '100px',
+          width: '100px',
+          backgroundColor: 'yellow' }
+      })
+    }
 
     render () {
       return (
-        <div onClick={this.clickHandler} style={this.state.style}>
+        <div onClick={this.clickHandler}
+          onMouseEnter={this.onMouseEnter}
+          onContextMenu={this.onContextMenu}
+          onDoubleClick={this.onDoubleClick}
+          onDragEnter={this.onDragEnter}
+          style={this.state.style}>
         </div>
       )
     }
