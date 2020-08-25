d279932a6bb51d843990631d27ed42e889afb0db
ariabjonesEDA <ariabjones@hotmail.com>
2020-08-25 16:52:43 +1200

    Add random color fn, add clickHandler

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,18 +1,32 @@
 import React from "react";
 
+const randomHexColor = () =>
+  `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
 class Pixel extends React.Component {
   state = {
     style: {
-      height: "20px",
-      width: "20px",
-      backgroundColor: "lightblue",
+      height: "100px",
+      width: "100px",
+      backgroundColor: randomHexColor(),
     },
   };
+
+  clickHandler = () => {
+    this.setState({
+      // ... update style here ...
+      style: {
+      backgroundColor: this.state.backgroundColor      
+    }
+  })
+}
 
   render() {
     return (
       //<>
-      <div style={this.state.style}> </div>
+      <div style={this.state.style} onClick={this.clickHandler} > 
+        </div>
+      
       //</>
     );
   }
