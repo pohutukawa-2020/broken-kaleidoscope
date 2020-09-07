e64635028756e07cb68609b61aefad7357308f48
Daryl Chen (Daz) <64760754+Daryl-creator@users.noreply.github.com>
2020-09-07 18:26:03 +1200

    made notes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 import Pixel from './Pixel.jsx'
 
-const App = () => {
+const App = () => { // this code generates an array which I have used to run the Pixel component in
    /* https://medium.com/@wisecobbler/4-ways-to-populate-an-array-in-javascript-836952aea79f */
-    const filledArray = Array(10000).fill(<Pixel/>)
+    const filledArray = Array(10000).fill(<Pixel/>) // this is the event delegation
     return filledArray
 }
 
--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 class Pixel extends React.Component {
+  // https://www.javatpoint.com/react-constructor
+  // The constructor is a method used to initialize an object's state in a class. It automatically called during the creation of an object in a class.
   constructor (props) {
+    // The constructor in a React component is called before the component is mounted. When you implement the constructor for a React component, you need to call super(props) method before any other statement. If you do not call super(props) method, this.props will be undefined in the constructor and can lead to bugs.
     super(props)
     const randomHexColor = () =>
   `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
-    this.state = {
+    this.state = { //https://reactjs.org/docs/faq-state.html
+      // In React, both this.props and this.state represent the rendered values, i.e. what’s currently on the screen.
       style: {
         height: '10px',
         width: '10px',
@@ -14,6 +18,7 @@ class Pixel extends React.Component {
   }
   render () {
     return (
+      // this refers to line 12 abv, this is what is being rendered and being dispalyed on the screen
       <div style={this.state.style}>    
       </div>
     )
